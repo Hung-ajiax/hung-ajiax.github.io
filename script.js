adde29e51bf471ac106fac38646d7fbd166c684c
Hung-ajiax <cindy7365934@gmail.com>
2025-03-18 10:34:44 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const btn = document.querySelector(".btn");
+    
+    btn.addEventListener("click", function() {
+        alert("歡迎來到我的個人網站！");
+    });
+});
